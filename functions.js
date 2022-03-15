11372eec12571aed0f21c47eb3be388b6eb3dcdd
MusiurAlam <musiur.opu@gmail.com>
2022-03-16 00:42:46 +0600

    first commit

---
+++ functions.js
@@ -0,0 +1,8 @@
+export const insertAt = (arr, index, value) => {
+    console.log(arr, index);
+    return [...arr.slice(0, index), value, ...arr.slice(index, arr.length)];
+}
+
+export const eraseAt = (arr, index) => {
+    return [...arr.slice(0, index-1), ...arr.slice(index+1, arr.length)]
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+import { eraseAt, insertAt } from "./functions";
+
+const opulib = {
+    arrayMethods: {
+        insertAt,
+        eraseAt
+    }
+}
+
+module.exports.opulib = opulib;
